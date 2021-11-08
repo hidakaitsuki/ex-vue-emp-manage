25a50ea4c5559aad942c2d75cacdf5da49a776f6
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-11-08 12:21:13 +0900

    registerAdminのルーター作成

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,6 +9,11 @@ const routes: Array<RouteConfig> = [
   //   path: "*",
   //   component: () => import("../components/LoginAdmin.vue"),
   // },
+
+   {
+    path: "registerAdmin",
+    component: () => import("../components/RegisterAdmin.vue"),
+  },
 ];
 
 const router = new VueRouter({
