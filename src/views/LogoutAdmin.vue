eaea915e8268f08e36f0c4dfc3e2c2fcaa80fbb0
hidakaitsuki <itsuki.hidaka@rakus-partners.co.jp>
2021-11-09 15:20:14 +0900

    logoutAdmin.vue作成完了

--- src/views/LogoutAdmin.vue
+++ src/views/LogoutAdmin.vue
@@ -1,13 +1,22 @@
 <template>
-  <div></div>
+  <div>dubby</div>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import axios from "axios";
+import Vue from "vue";
+import Component from "vue-class-component";
 
+@Component
+export default class LoginAdmin extends Vue {
+  /**
+   * ログアウトする
+   */
+  async created(): Promise<void> {
+    await axios.get("http://34.221.70.66:8080/ex-emp-api/logout");
+    this["$router"].push("/loginAdmin");
+  }
 }
 </script>
 
-<style>
-
-</style>
+<style></style>
